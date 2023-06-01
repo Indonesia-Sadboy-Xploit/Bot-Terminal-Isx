3b09fd122fad73340b4fe882224fe96353c57c19
INDONESIA SADBOY XPLOIT <87993609+Indonesia-Sadboy-Xploit@users.noreply.github.com>
2023-06-02 04:41:47 +0800

    Add files via upload

---
+++ Bot-Terminal-Isx.sh
@@ -0,0 +1,77 @@
+default="\033[00m"
+hijau="\033[32m"
+
+
+figlet "SimiSimi"
+echo $hijau"Script Ini Hanya Dibuat Untuk Kaum Jomblo Mwehehe!"
+echo "Jangan Pada Baper Ya Ngentot"
+echo "----------------------------------"
+echo "Script By Foundation Security Zone"
+echo "Website: https://foundation-security-zone-website.000webhostapp.com/"
+echo "Github: https://github.com/Foundation-Security-Zone/"
+echo "----------------------------------"
+echo "1)Chat Simi Simi Version (1) Terbatas"
+echo "2)Chat Simi Simi Version (2) No Terbatas"
+echo "3)Chat Pacar"
+echo "4)Tentang Script Ini"
+echo "5)Exit"
+read -p "Silahkan Pilih!!: " pil;
+echo "----------------------------------"
+if [ $pil = "1" ];
+  then
+echo -n "Masukan Nama Lo!:"
+read namaLo
+read -p "$namaLo: " pil
+echo "Simi: Hai Juga"
+read -p "$namaLo: " pil
+echo "Simi: Kenapa Lo?"
+read -p "$namaLo: " pil
+echo "Simi: Idiot Lo!"
+read -p "$namaLo: " pil
+echo "Simi: Apa Mau Aku Pukul Apa Cium?"
+read -p "$namaLo: " pil
+echo "Simi: Mau Jadi Pacarku?"
+read -p "$namaLo: " pil
+echo "Simi: Lo Ini Aneh Kontol"
+read -p "$namaLo: " pil
+echo "Simi: Bego"
+read -p "$namaLo: " pil
+echo "Simi: Ah Bodo Tutup"
+read -p "$namaLo: " pil
+echo "Simi: Bodo"
+read -p "$namaLo: " pil
+echo "Simi: Yahahaha"
+read -p "$namaLo: " pil
+echo "Simi: Nama Lo Siapa Wibu?"
+read -p "$namaLo: " pil
+echo "Simi: Huu Gak Jelas Lo Wibu"
+read -p "$namaLo: " pil
+echo "Simi: Yahahaha Wibu"
+read -p "$namaLo: " pil
+while [ true ]
+do
+echo "Stop Chat Nya Ngab Waktunya Terbatas!"
+done
+fi
+if [ $pil = "2" ];
+  then
+echo -n "Masukan Nama Lo!:"
+read you
+while [ repeat ]
+do
+read -p "$you: " pil
+   echo "Bot: "$(shuf -n1 list.txt)
+done
+fi
+if [ $pil = "3" ];
+  then
+echo "Belum Ada Ngabbb!"
+fi
+if [ $pil = "4" ];
+  then
+echo "Script Ini Hanya Untuk Hiburan Saja Kepada Kaum Jomblo Maupun Nolep Jadi Jangan Baper Ya Tod :v"
+fi
+if [ $pil = "5" ];
+  then
+echo "Exit"
+fi
